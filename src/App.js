f0cf6d3031ac6995b2769f4e1efa0f41d35fd0da
Edgardo <andrescarrasquel97@gmail.com>
2022-01-04 22:28:01 -0400

    Primer commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = ()=>{
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="app_container">
+        <header className="app_header">
+          <h1>To-Do List</h1>
+
+          <form className="todo_form">
+
+              <label className="label_form"> Description: </label>
+              <input className="input_text" type="text" maxlength="50"/>
+              <button className="form_button" >
+                Submit
+              </button>
+
+          </form>
+
+        </header>
+
+        <article className="list_container">
+          <div className="list_item">
+            <input type="checkbox"/>
+            <p>
+              Todo Texto
+            </p>
+            <time>
+              00:00
+            </time>
+          </div>
+          <div className="list_item">
+            <input type="checkbox"/>
+            <p>
+              Todo Texto
+            </p>
+            <time>
+              00:00
+            </time>
+          </div>
+        </article>
+
+        <footer className="app_footer">
+          <p>total: 0</p>
+          <span>
+            Filter by:
+            <select className="app_select" name="select">
+              <option value="all" selected>All</option>
+              <option value="checked">Checked</option>
+              <option value="unchecked">Unchecked</option>
+            </select>
+          </span>
+        </footer>
+
+      </div>
+
     </div>
   );
 }
